f4744e0922d22158e5bd2c8ec7878574941bc954
AnimeSenseiNetwork <131219940+AnimeSenseiNetwork@users.noreply.github.com>
2024-12-29 20:57:47 +0530

    Update config.py

--- config.py
+++ config.py
@@ -27,7 +27,7 @@ DB_URI = os.environ.get("DATABASE_URL", "mongodb+srv://animesensei:animesensei11
 DB_NAME = os.environ.get("DATABASE_NAME", "filesharexbot")
 
 # Put 0 on on that which you don't want to enable
-FORCE_CHANNEL = int(os.environ.get("FORCE_CHANNEL", "-1002237693100"))
+FORCE_CHANNEL = int(os.environ.get("FORCE_CHANNEL", "-1002491040507"))
 FORCE_CHANNEL2 = int(os.environ.get("FORCE_CHANNEL2", "-1001535383648"))
 REQUEST_CHANNEL = int(os.environ.get("REQUEST_CHANNEL", "-1001133546052"))
 
