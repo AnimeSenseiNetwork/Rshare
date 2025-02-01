442e789664ff73c9280f20ae086fc5b72991e446
AnimeSenseiNetwork <131219940+AnimeSenseiNetwork@users.noreply.github.com>
2025-02-01 20:00:17 +0530

    Update config.py

--- config.py
+++ config.py
@@ -28,7 +28,7 @@ DB_NAME = os.environ.get("DATABASE_NAME", "filesharexbot")
 
 # Put 0 on on that which you don't want to enable
 FORCE_CHANNEL = int(os.environ.get("FORCE_CHANNEL", "-1002491040507"))
-FORCE_CHANNEL2 = int(os.environ.get("FORCE_CHANNEL2", "-1002038620816"))
+FORCE_CHANNEL2 = int(os.environ.get("FORCE_CHANNEL2", "-1002211677343"))
 REQUEST_CHANNEL = int(os.environ.get("REQUEST_CHANNEL", "-1002358818038"))
 
 #TXT
