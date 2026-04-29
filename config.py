6068274c47ec5589b57508c70571007407e7c12a
AnimeSenseiNetwork <131219940+AnimeSenseiNetwork@users.noreply.github.com>
2026-04-29 16:39:14 +0530

    Update config.py

--- config.py
+++ config.py
@@ -5,7 +5,7 @@ from logging.handlers import RotatingFileHandler
 
 
 #Bot token @Botfather
-TG_BOT_TOKEN = os.environ.get("TG_BOT_TOKEN", "6813308007:AAEccE2wx1uU1OpfthrnGyKowaqGc3N4LMw")
+TG_BOT_TOKEN = os.environ.get("TG_BOT_TOKEN", "6501228626:AAGGWAx5k5aYJWRgjE0xYH-xpgoJwCnySHs")
 
 #Your API ID from my.telegram.org
 APP_ID = int(os.environ.get("APP_ID", "26376042"))
@@ -20,7 +20,7 @@ CHANNEL_ID = int(os.environ.get("CHANNEL_ID", "-1002097979375"))
 OWNER_ID = int(os.environ.get("OWNER_ID", "2036803347"))
 
 #Port
-PORT = os.environ.get("PORT", "8050")
+PORT = os.environ.get("PORT", "8030")
 
 #Database 
 DB_URI = os.environ.get("DATABASE_URL", "mongodb+srv://animesensei:animesensei11@animesensei.o6zhyp8.mongodb.net/?retryWrites=true&w=majority")
